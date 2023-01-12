acd55c5f86e1eb56c547b4c4d383312a82939696
antigravkids <hcarter333@gmail.com>
2023-01-12 11:57:18 -0800

    fixes typo

--- date_format.py
+++ date_format.py
@@ -1,5 +1,5 @@
 import datetime
 e='%H%Mz %d %b'
 d=datetime.datetime.strptime("2337z 10 Jan", e)
-date=date.replace(2023)
+d=d.replace(2023)
 print(datetime.datetime.strftime(d, '%Y/%m/%d %H:%M:%S'))
