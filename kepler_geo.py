56073a0e4b37cdea022ed291dfd5faa308edbb73
antigravkids <hcarter333@gmail.com>
2023-01-17 04:22:56 -0800

    Writes numbers as strings

--- kepler_geo.py
+++ kepler_geo.py
@@ -32,5 +32,5 @@ print("id,geometry,timestamp,dB,frequency,Spotter")
 for i in spots:
     print(i+',"{""type"":""LineString"",""coordinates"":[[-122.42299,37.72286],['+\
     	geo_data[spots[i][0]][7]+","+geo_data[spots[i][0]][6]+\
-    	']]}","'+spots[i][5]+'"'+','+spots[i][3]+','+spots[i][1]+','+spots[i][0])
+    	']]}","'+spots[i][5]+'"'+','+str(spots[i][3])+','+str(spots[i][1])+','+spots[i][0])
 
