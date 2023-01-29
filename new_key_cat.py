7a5ab20fd758b0875c90fac6abbce2e8ef228d2c
Carter <hcarter@mgc.mentorg.com>
2023-01-29 09:05:18 -0800

    code to address unique key entries in #9 also sets up testing framework

---
+++ new_key_cat.py
@@ -0,0 +1,26 @@
+import sys
+#open the existing presentation file
+
+#Make a list of all the keys
+#data_file will normally be 
+#rm_rnb_history_pres.cs
+def make_key_list(data_file):
+    list2 = []
+    with open(data_file) as f:
+        for row in f:
+            #there are non-separator commas, but since the key is in the 
+            #first field, we'll be fine
+            list2.append(row.split(",")[0])
+    return list2
+
+#Now, as each line comes in from stdin only output it 
+#if its key isn't in the list'
+def new_key_cat(test_file):
+    existing_key_list = make_key_list("rm_rnb_history_pres.csv")
+    if(test_file==""):
+        f = sys.stdin
+    else:
+        f = open(test_file)
+    for line in f:
+        if(line.split(",")[0] not in existing_key_list):
+            print(line)
---
+++ test_new_key_cat_1.py
@@ -0,0 +1,21 @@
+from new_key_cat import make_key_list
+from new_key_cat import new_key_cat
+
+def test_answer():
+    #list of keys
+    key_list = []
+    key_list = make_key_list("new_key_cat_test1.csv")
+    assert("1516323033" in key_list)
+    assert("1513727969" in key_list)
+    assert("1516323033" in key_list)
+
+def test_new_key_cat_2(capsys):  # or use "capfd" for fd-level
+    #call the method with a file that has both new keys and 
+    #existing keys
+    new_key_cat("new_key_cat_test2.csv")
+    captured = capsys.readouterr()
+    for entry in make_key_list("new_key_cat_test1.csv"):
+        assert captured.out.find(entry) == -1
+    #Also check that the new key lines were output
+    assert captured.out.find("2,") != -1
+    assert captured.out.find("3,") != -1
