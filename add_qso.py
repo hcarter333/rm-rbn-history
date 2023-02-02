8fd23fa42854714721a8451cfbf12080f5eeefbf
Carter <hcarter@mgc.mentorg.com>
2023-02-02 10:34:45 -0800

    adds basis for adding qsos #12

---
+++ add_qso.py
@@ -0,0 +1,14 @@
+import sys
+
+#Read each line in the qso_file
+#format it as kepler_geo and write it to stdout using print
+#generate random 32 bit key
+def add_qso(qso_file, key=77):
+    f = open(qso_file)
+    if(key==77):
+        key = randrange(0,4294967295)
+    for line in f:
+        fields = line.split(",")
+        print(str(key)+',"{""type"":""LineString"",""coordinates"":[['+ fields[0] +\
+          ',' + fields[1] + '],['+fields[2]+","+fields[3]+']]}","'+fields[4]+'"'+\
+          ','+fields[5]+','+fields[6]+','+fields[7])
---
+++ test_add_qso.py
@@ -0,0 +1,18 @@
+from add_qso import add_qso
+
+def test_add_qso_1(capsys):  # or use "capfd" for fd-level
+    #call the method to add qsos
+    add_qso("test_add_qso.txt", 7)
+    captured = capsys.readouterr()
+    #check that the output line was formatted correctly
+    captured_fields = captured.out.split(",")
+    assert captured.out=='7,"{""type"":""LineString"",""coordinates"":[[-106.579229, 32.380401],[-94.6250,38.6458]]}","2023/01/31 11:47:00",7,14057.6,KTEST\n'
+ 
+def test_add_qso_2(capsys):  # or use "capfd" for fd-level
+    #call the method to add qsos
+    add_qso("test_add_qso2.txt", 7)
+    captured = capsys.readouterr()
+    #check that the output line was formatted correctly
+    #Uses output directly from generating google sheet
+    captured_fields = captured.out.split(",")
+    assert captured.out=='7,"{""type"":""LineString"",""coordinates"":[[-106.578838,32.382328],[-97.0957613946923,30.0488933199194]]}","2023/01/29 17:32",0,14058.3,KF9RX\n'
