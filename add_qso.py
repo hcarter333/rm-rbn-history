835e3aef1fabc614e11e820f9729839cec8e6767
hcarter333 <hcarter333@gmail.com>
2023-02-02 11:41:20 -0800

    key per entry_qso.py

--- add_qso.py
+++ add_qso.py
@@ -6,11 +6,11 @@ import random
 #generate random 32 bit key
 def add_qso(qso_file, key=77):
     f = open(qso_file)
-    if(key==77):
-        key = random.randrange(0,4294967295)
     for line in f:
         fields = line.split(",")
         if(len(fields)==8):
+            if(key==77):
+                key = random.randrange(0,4294967295)
             print(str(key)+',"{""type"":""LineString"",""coordinates"":[['+ fields[0] +\
               ',' + fields[1] + '],['+fields[2]+","+fields[3]+']]}","'+fields[4]+'"'+\
               ','+fields[5]+','+fields[6]+','+fields[7])
