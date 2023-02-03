a36fac20c173d97a4b54b119e2d11eb5d0f02d0a
hcarter333 <hcarter333@gmail.com>
2023-02-03 08:32:49 -0800

    first prototypes for #13

---
+++ qso_spot_kml.py
@@ -0,0 +1,32 @@
+import sys
+import random
+
+#Read each line in the qso_file
+#format it as kepler_geo and write it to stdout using print
+#generate random 32 bit key
+def qso_spot_kml(qso_file, key=77):
+    linestart = "<Placemark> <LineString><coordinates>"
+    lineend = "</coordinates></LineString>"
+    qso_style = "<Style><LineStyle><color>#ff00ff00</color><width>3</width></LineStyle></Style>"
+    spot_style = "<Style><LineStyle><color>#ffff0000</color><width>3</width></LineStyle></Style>"
+    place_end = "</Placemark>"
+    f = open(qso_file)
+    #output the standard header
+    print('<?xml version="1.0" encoding="UTF-8"?>')
+    print('<kml xmlns="http://earth.google.com/kml/2.0"> <Document>')
+    for line in f:
+        fields = line.split(",")
+        if(len(fields)==8):
+                print(linestart)
+                print(fields[0]+","+fields[1]+",0.")
+                print(fields[2]+","+fields[3]+",0.")
+                print(lineend)
+                if(fields[5] == "0"):
+                    #set for QSO line color
+                    print(qso_style)
+                else:
+                    #set for spot line color (RBN)
+                    print(spot_style)
+                print(place_end)
+    #output file footer
+    print('</Document> </kml>')
