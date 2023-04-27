dcb502fc03a3eca85a2f37e4f4c6f6d912293d3d
hcarter333 <hcarter333@gmail.com>
2023-04-27 15:46:42 -0700

    adds address method for #34

--- auto_geo_update.py
+++ auto_geo_update.py
@@ -13,5 +13,25 @@ def get_qrz_session(username):
     sess_id = root.find('{http://xmldata.qrz.com}Session/{http://xmldata.qrz.com}Key')
     return sess_id
 
+def get_qrz_call_geo_address(callsign):
+    #get the session id
+    sess_id = get_qrz_session("KD0FNR")
+    #get the address for the callsign
+    r = requests.get('https://xmldata.qrz.com/xml/current/?s='+sess_id.text+';callsign='+callsign)
+    root = ET.fromstring(r.text)
+    #now, get addr1, addr2, and state
+    addr1 = root.find('{http://xmldata.qrz.com}Callsign/{http://xmldata.qrz.com}addr1')
+    addr2 = root.find('{http://xmldata.qrz.com}Callsign/{http://xmldata.qrz.com}addr2')
+    state = root.find('{http://xmldata.qrz.com}Callsign/{http://xmldata.qrz.com}state')
+    address_geo = addr1.text + ',' + addr2.text + ',' + state.text
+    address_geo = address_geo.replace(' ','+')
+    return address_geo
+
+#get session id
+#read in csv file with 
+#call,rx_rst,tx_rst,date_time
+#get address for callsign
+#format address into plus string
+#get coordinates for address
 
 
--- test_auto_geo_update.py
+++ test_auto_geo_update.py
@@ -1,6 +1,11 @@
 from auto_geo_update import get_qrz_session
+from auto_geo_update import get_qrz_call_geo_address
 
 def test_get_qrz_session():
     #call with env_variable password
     sess_id = get_qrz_session("KD0FNR")
     assert sess_id != None
+
+def test_get_qrz_call_geo_address():
+    addr_geo = get_qrz_call_geo_address("NU6XB")
+    assert addr_geo == "506+Cory+Hall,Berkeley,CA"
