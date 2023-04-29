d409deae567f9167167e23085fbb0cc8a6b1830a
hcarter333 <hcarter333@gmail.com>
2023-04-28 20:45:03 -0700

    moves to separate file for testing

---
+++ expe_kml_defs.py
@@ -0,0 +1,52 @@
+import sys
+import datetime
+
+def expe_kml_per_line(lng, lat, fields, begin_timestamp, end_timestamp):
+    try:
+        line_date = datetime.datetime.strptime(fields[5], '%Y/%m/%d %H:%M:%S')
+    except:
+        try:
+            line_date = datetime.datetime.strptime(fields[5], '%Y/%m/%d %H:%M')
+        except:
+            print("Error parsing date input:",sys.exc_info())
+            print("Bad line_date")
+            print(fields[5])
+            sys.exit(1)
+
+ #       line_date = datetime.datetime.strptime(fields[5], '%Y/%m/%d %H:%M:%S')
+    if((line_date >= begin_timestamp) and (line_date <= end_timestamp)):
+         output_string = str(lng)+','+str(lat)+','+fields[3]+','+fields[4]+\
+            ','+fields[5]+','+fields[6]+','+fields[7]+','+fields[8]
+         sys.stdout.write(output_string)
+         #print("Here's the first output string")
+         #print(output_string)
+         return output_string
+    
+
+
+def expe_kml(lng, lat, begin_timestamp, end_timestamp, qso_list=[]):
+    result = []
+    if(len(qso_list) == 0):
+        f = open('rm_rnb_history_pres.csv')
+        firstline = 1
+        print("qso processing in file")
+        for line in f:
+            #throw away the first line
+            fields = line.split(",")
+            if((firstline != 1) and (len(fields)==9)):
+                qso_out = expe_kml_per_line(lng, lat, fields, begin_timestamp, end_timestamp)
+                if(qso_out != None):
+                    result.append(qso_out)
+            else:
+              firstline = 0
+    else:
+        for qso in qso_list:
+            #construct fields (the first one is the unused random key)
+            rx_loc=qso[3].split(",")
+            fields = [qso[0],str(qso[1]),str(qso[2]),rx_loc[0],rx_loc[1],\
+                      qso[4].strftime("%Y/%m/%d %H:%M:%S"),qso[5],'14058.4',qso[6]]
+            qso_out = expe_kml_per_line(lng, lat, fields, begin_timestamp, end_timestamp)
+            #print("pass this on " + qso_out)
+            if(qso_out != None):
+                result.append(qso_out)
+    return result
