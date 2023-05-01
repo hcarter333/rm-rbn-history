43496cb75e625df178f4234ce0f71134d8e87622
hcarter333 <hcarter333@gmail.com>
2023-05-01 05:14:58 -0700

    Removes RBN stdout per #39

--- expe_kml_defs.py
+++ expe_kml_defs.py
@@ -2,7 +2,7 @@ import sys
 import datetime
 from auto_geo_vars import kml_title, tx_lng, tx_lat
 
-def expe_kml_per_line(lng, lat, fields, begin_timestamp, end_timestamp):
+def expe_kml_per_line(lng, lat, fields, begin_timestamp, end_timestamp, out):
     try:
         line_date = datetime.datetime.strptime(fields[5], '%Y/%m/%d %H:%M:%S')
     except:
@@ -18,7 +18,8 @@ def expe_kml_per_line(lng, lat, fields, begin_timestamp, end_timestamp):
     if((line_date >= begin_timestamp) and (line_date <= end_timestamp)):
          output_string = str(lng)+','+str(lat)+','+fields[3]+','+fields[4]+\
             ','+fields[5]+','+fields[6]+','+fields[7]+','+fields[8]
-         sys.stdout.write(output_string)
+         if(out == True):
+             sys.stdout.write(output_string)
          #print("Here's the first output string")
          #print(output_string)
          return output_string
@@ -35,7 +36,8 @@ def expe_kml(lng, lat, begin_timestamp, end_timestamp, qso_list=[]):
         #throw away the first line
         fields = line.split(",")
         if((firstline != 1) and (len(fields)==9)):
-            qso_out = expe_kml_per_line(lng, lat, fields, begin_timestamp, end_timestamp)
+            qso_out = expe_kml_per_line(lng, lat, fields, begin_timestamp, 
+                                        end_timestamp, False)
             if(qso_out != None):
                 result.append(qso_out)
         else:
@@ -46,7 +48,8 @@ def expe_kml(lng, lat, begin_timestamp, end_timestamp, qso_list=[]):
         rx_loc=qso[3].split(",")
         fields = [qso[0],str(qso[1]),str(qso[2]),rx_loc[0],rx_loc[1],\
                   qso[4].strftime("%Y/%m/%d %H:%M:%S"),qso[5],'14058.4',qso[6]]
-        qso_out = expe_kml_per_line(lng, lat, fields, begin_timestamp, end_timestamp)
+        qso_out = expe_kml_per_line(lng, lat, fields, begin_timestamp, 
+                                    end_timestamp, True)
         #print("pass this on " + qso_out)
         if(qso_out != None):
             result.append(qso_out)
