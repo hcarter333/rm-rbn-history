0ad6ee3699583d537b88d01939ec22bd2893f16f
antigravkids <hcarter333@gmail.com>
2023-08-27 20:44:59 +0000

    adds dictionary of useful frquencies on the Rockmite 20

--- rbn_telnet.py
+++ rbn_telnet.py
@@ -7,14 +7,27 @@ tn = telnetlib.Telnet("telnet.reversebeacon.net", 7000)
 data = tn.read_until(b"Please enter your call:")
 command = "kd0fnr\r\n"
 tn.write(command.encode("utf-8"))
+#frequencies we can use
+#14058.0 14058.1 14058.2 14057.5 14057.4 14057.6 14057.9 
+useful_freqs = {"14058.0": -1,
+                "14058.1": -1,
+                "14058.2": -1, 
+                "14057.5": -1,
+                "14057.4": -1,
+                "14057.6": -1,
+                "14057.9": -1}
 try:
     while True:
         data = tn.read_until(b"\n")  # Read data until a newline character
         #print(data)
         # Assuming the callsign is enclosed in square brackets, e.g., [CALLSIGN]
-        frequency_find = data.find(b"14058.")
+        print_freq = 2;
+        for frequ in useful_freqs:
+            print_freq = data.find(bytes(frequ, encoding='utf8'))
+            if(print_freq != -1):
+                break
         
-        if frequency_find != -1:
+        if print_freq != -1:
             print(data)
 
 except KeyboardInterrupt:
