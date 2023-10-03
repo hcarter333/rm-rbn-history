492b77c8ad8b6baf74f898cf48ce874aaf2f9938
antigrav_kids <hcarter333@gmail.com>
2023-10-03 05:01:52 -0700

    first implementation of frequency range #64

--- rbn_telnet.py
+++ rbn_telnet.py
@@ -1,35 +1,48 @@
 import telnetlib
+import argparse
 
 #host = "your_host_address"
 #port = 23  # The telnet port
+
+#Accept frequency range per #64
+parser = argparse.ArgumentParser(
+                    prog='rbn_telnet.py',
+                    description='Outputs CQs detected by the RBN over a give freqency range',
+                    epilog='Text at the bottom of help')
+parser.add_argument('-b', type=float, dest="start_freq", default=14057.3,\
+                    help="start of frquency range to watch")
+parser.add_argument('-e', type=float, dest="end_freq", default=14058.2,\
+                     help="start of frquency range to watch")
+
+args = parser.parse_args()
 
 tn = telnetlib.Telnet("telnet.reversebeacon.net", 7000)
 data = tn.read_until(b"Please enter your call:")
 command = "kd0fnr\r\n"
 tn.write(command.encode("utf-8"))
-#frequencies we can use
-#14058.0 14058.1 14058.2 14057.5 14057.4 14057.6 14057.9 
-useful_freqs = {"14058.0": -1,
-                "14058.1": -1,
-                "14058.2": -1, 
-                "14057.5": -1,
-                "14057.4": -1,
-                "14057.3": -1,
-                "14057.6": -1,
-                "14057.9": -1}
+
 try:
     while True:
         data = tn.read_until(b"\n")  # Read data until a newline character
         # Search incoming data for the list of frequencies the Rockmite can 
         #use to transmit and receive
-        print_freq = -1;
-        for frequ in useful_freqs:
-            print_freq = data.find(bytes(frequ, encoding='utf8'))
-            if(print_freq != -1):
-                break
-        
-        if print_freq != -1:
-            print(data)
+        #dump the fields split on space
+        #print(data)
+        fields = data.split()
+        #print(str(len(fields)))
+        #print(fields)
+        if(len(fields) > 7):
+            #print(fields[3])
+            #print(str(float(fields[3]) + 1))
+            try:
+                curr_freq = float(fields[3])
+                #print("freq " + str(fields[3]))
+                if(curr_freq >= args.start_freq and curr_freq <= args.end_freq):
+                    print(data)
+            except Exception as error:
+                print(fields[3][2:])
+                print("found and exception " + str(error))
+                continue
 
 except KeyboardInterrupt:
     print("Exiting...")
