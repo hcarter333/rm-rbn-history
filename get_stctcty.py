65aa6cad0bc51ccb4737e3265bc07a4e60d0c246
antigrav_kids <hcarter333@gmail.com>
2023-10-09 22:08:03 -0700

    first complete implementation

--- get_stctcty.py
+++ get_stctcty.py
@@ -3,13 +3,15 @@ import requests
 import json
 
 #Datasette qeury to retrieve QSOs
-qso_rx_geo = requests.get('http://127.0.0.1:8001/rm_toucans_23_10_07.json?sql=\
-                          select+Spotter%2C+rx_lng%2C+rx_lat+from+rm_rnb_history_pres+where+\
+qso_rx_geo = requests.get('http://127.0.0.1:8001/rm_toucans_32_10_09.json?sql=\
+                          select+Spotter%2C+rx_lng%2C+rx_lat+%2C+id+from+rm_rnb_history_pres+where+\
                           %22dB%22+%3E+%3Ap0+order+by+rowid&p0=99')
-#print(qso_rx_geo.text)
 q = json.loads(qso_rx_geo.text)
+#Dictionary to hold county entires
+id_to_county = {}
+id_to_state = {}
+first_line = False
 for result in q["rows"]:
-    #print(result)
     lng = result[1]
     lat = result[2]
     ds_geo_json = requests.get('http://127.0.0.1:8001/counties.json?sql=select%0D%0A++STATEFP+as+\
@@ -23,8 +25,19 @@ for result in q["rows"]:
                                SpatialIndex%0D%0A++++where%0D%0A++++++f_table_name+%3D+%27counties%27%0D%0A++++++and\
                                +search_frame+%3D+MakePoint%28cast%28%3Alongitude+as+float%29%2C+cast%28%3Alatitude+\
                                as+float%29%29%0D%0A++%29%0D%0Alimit%0D%0A++1&longitude='+str(lng)+'&latitude='+str(lat))
-    #print(ds_geo_json.text)
     z = json.loads(ds_geo_json.text)
     for localities in z["rows"]:
-        print(result[0] + ':' + localities[1] + ' ' + localities[3])
-    
+        id_to_county[result[3]] = localities[3]
+        id_to_state[result[3]] = localities[1]
+#now that we have all the available states and counties, update rm_rbn_history_pres.csv
+csv_file='rm_rnb_history_pres.csv'
+f = open(csv_file)
+for line in f:
+    line = line.replace("\n","")
+    fields = line.split(",")
+    if(fields[0] in id_to_county):
+        line_out = line + ",USA,"+id_to_state[str(fields[0])]+","+id_to_county[str(fields[0])]
+        print(line_out)
+    else:
+       print(line)
+            
