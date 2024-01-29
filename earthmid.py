a8a1119f6246a440e91e8c9f7e762838a92e2d28
antigrav_kids <hcarter333@gmail.com>
2024-01-29 08:01:03 -0800

    allows integer path length divisions

--- earthmid.py
+++ earthmid.py
@@ -26,22 +26,14 @@ def spherical_lng(x,y,z):
     return (math.atan2(y, x)*rad2deg) # return degrees
 
 def midpoint_lat(f0,l0, f1,l1):
-    #get the x y and z values
-    x_0 = cartesian_x(f0,l0)
-    y_0 = cartesian_y(f0,l0)
-    z_0 = cartesian_z(f0,l0)
-    x_1 = cartesian_x(f1,l1)
-    y_1 = cartesian_y(f1,l1)
-    z_1 = cartesian_z(f1,l1)
-   
-    x_mid = (x_0+x_1)/2
-    y_mid = (y_0+y_1)/2
-    z_mid = (z_0+z_1)/2
-
-    return spherical_lat(x_mid, y_mid, z_mid)
+    return partial_path_lat(f0,l0, f1,l1,2)
 
 def midpoint_lng(f0,l0, f1,l1):
     #get the x y and z values
+    return partial_path_lng(f0,l0, f1,l1,2)
+
+def partial_path_lat(f0,l0, f1,l1, parts):
+    #get the x y and z values
     x_0 = cartesian_x(f0,l0)
     y_0 = cartesian_y(f0,l0)
     z_0 = cartesian_z(f0,l0)
@@ -49,11 +41,28 @@ def midpoint_lng(f0,l0, f1,l1):
     y_1 = cartesian_y(f1,l1)
     z_1 = cartesian_z(f1,l1)
    
-    x_mid = (x_0+x_1)/2
-    y_mid = (y_0+y_1)/2
-    z_mid = (z_0+z_1)/2
+    x_mid = (x_0+x_1)/parts
+    y_mid = (y_0+y_1)/parts
+    z_mid = (z_0+z_1)/parts
+
+    return spherical_lat(x_mid, y_mid, z_mid)
+
+def partial_path_lng(f0,l0, f1,l1, parts):
+    #get the x y and z values
+    x_0 = cartesian_x(f0,l0)
+    y_0 = cartesian_y(f0,l0)
+    z_0 = cartesian_z(f0,l0)
+    x_1 = cartesian_x(f1,l1)
+    y_1 = cartesian_y(f1,l1)
+    z_1 = cartesian_z(f1,l1)
+   
+    x_mid = (x_0+x_1)/parts
+    y_mid = (y_0+y_1)/parts
+    z_mid = (z_0+z_1)/parts
 
     return spherical_lng(x_mid, y_mid, z_mid)
+
+
 
 #half_lat = midpoint_lat(37.72286,-122.42299, 41.0625,-112.0417)
 #half_lng = midpoint_lng(37.72286,-122.42299, 41.0625,-112.0417)
