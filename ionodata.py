128f38c7ed53990d3317992b1d5bf024d1e0bf17
antigrav_kids <hcarter333@gmail.com>
2024-06-05 09:53:06 -0700

    fixes optional ionosonde station arg; adds may 31 qsos in the afternoon of may 30

--- ionodata.py
+++ ionodata.py
@@ -6,7 +6,7 @@ import urllib3
 def get_f2m(start_time, end_time, station = "EA653"):
 #def get_f2m():
     hmF2 = "5"
-    station = "EA653"
+    #station = "EA653"
     ptargstation = "PA836"
     #2023/02/08 01:34:00
     start_date_win = start_time.strftime("%Y.%m.%d %H:%M:%S")
@@ -19,7 +19,7 @@ def get_f2m(start_time, end_time, station = "EA653"):
     urllib3.disable_warnings()
     iono_url = "https://lgdc.uml.edu/common/DIDBGetValues?ursiCode="+station+"&charName=hF2,hmF2&fromDate=" + start_date_win + \
                         "&toDate=" + end_date_win
-    #print("iono_url = " + iono_url)
+    print("iono_url = " + iono_url)
     #                                                                                                        2023.02.08%2002:50:00
     iodata=requests.get(iono_url, verify=False)
     #print(iodata.text)
@@ -32,6 +32,7 @@ def get_f2m(start_time, end_time, station = "EA653"):
             #We've arrived at the data
             #Split on space and print the max F2
             data_fields = data_line.split()
+            print(data_fields)
             hmF2 = data_fields[4]
             #print("hmF2 " + data_fields[4])
             break
